377dca9e1856ed918bd0f3bca1c5a135e3f56191
Alexander Olsson <noseglid@gmail.com>
2015-11-18 22:15:12 +0100

    Added support for awaiting build targets
    Since build targets are now scanned asynchronously, it is
    important for atom build itself as well as any build provider
    to be able to wait for this to be done before testing stuff.

--- index.js
+++ index.js
@@ -37,6 +37,26 @@ module.exports = {
     this.dispatchKeyboardEvent(element, 'keyup', eventArgs);
   },
 
+  awaitTargets: function () {
+    return new Promise(function(resolve, reject) {
+      let ev = atom.notifications.onDidAddNotification((notification) => {
+        if (notification.getMessage() !== 'Build targets parsed.') {
+          return;
+        }
+        let nots = atom.notifications.notifications;
+        nots.splice(nots.indexOf(notification), 1);
+
+        ev.dispose();
+        resolve();
+      });
+    });
+  },
+
+  refreshAwaitTargets: function () {
+    atom.commands.dispatch(atom.views.getView(atom.workspace), 'build:refresh-targets');
+    return this.awaitTargets();
+  },
+
   vouch: function vouch(fn /* args... */) {
     var args = Array.prototype.slice.call(arguments, 1);
     return new Promise(function(resolve, reject) {
